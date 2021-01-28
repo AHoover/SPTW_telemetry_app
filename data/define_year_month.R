29e97a9d55e8c22a52da5187513d7e803b77799d
AHoover <aimhoover@gmail.com>
2021-01-28 16:12:20 +0000

    Initial data configuration for app

---
+++ data/define_year_month.R
@@ -0,0 +1,4 @@
+define_year_month = function(yearB,monthB) {
+  year <<- yearB # Enters 'yearB' as called in the function into the R global environment
+  month <<- monthB # Enters 'monthB' as called in the function into the R global environment
+  }
